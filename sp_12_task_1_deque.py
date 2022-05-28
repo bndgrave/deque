581008fa7d810064e97d8015cd9e743137105404
Дмитрий Ошмарин <bndgrave1@gmail.com>
2022-05-29 00:22:38 +0500

    Commit 3: Basic logic of Deque realized

--- sp_12_task_1_deque.py
+++ sp_12_task_1_deque.py
@@ -1,21 +1,33 @@
 class Deque:
     def __init__(self):
-        pass
+        self.items = []
 
-    def push_front(self):
-        pass
+    def push_front(self, item):
+        self.items.insert(0, item)
 
-    def push_back(self):
-        pass
+    def push_back(self, item):
+        self.items.append(item)
 
     def pop_front(self):
-        pass
+        try:
+            print(self.items.pop())
+        except IndexError:
+            print('Удаление невозможно, дек пуст!')
 
     def pop_back(self):
-        pass
+        try:
+            print(self.items.pop())
+        except IndexError:
+            print('Удаление невозможно, дек пуст!')
 
-def main(input_data):
-    pass
+# def main(input_data):
+#     pass
 
 if __name__ == '__main__':
-    pass
+    d = Deque()
+    print(d.items)
+    [d.push_front(elem) for elem in range(2)]
+    [d.push_back(elem) for elem in range(2)]
+    print(d.items)
+    [d.pop_back() for ind in range(len(d.items)+1)]
+    print(d.items)
