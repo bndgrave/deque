d23bfe60b8091829bd8f17277b58cfec8d428dbd
Дмитрий Ошмарин <bndgrave1@gmail.com>
2022-05-28 23:58:47 +0500

    Commit 2: Basics arranged

---
+++ sp_12_task_1_deque.py
@@ -0,0 +1,21 @@
+class Deque:
+    def __init__(self):
+        pass
+
+    def push_front(self):
+        pass
+
+    def push_back(self):
+        pass
+
+    def pop_front(self):
+        pass
+
+    def pop_back(self):
+        pass
+
+def main(input_data):
+    pass
+
+if __name__ == '__main__':
+    pass
