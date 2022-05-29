3467876f528e8dc90c8585fc3ad7885f2bf0e542
Дмитрий Ошмарин <bndgrave1@gmail.com>
2022-05-29 16:47:25 +0500

    Commit 5: Completed

--- sp_12_task_1_deque.py
+++ sp_12_task_1_deque.py
@@ -14,52 +14,53 @@ class Deque:
             self.items[self.first] = item
             self.first = (self.first - 1) % self.max_items
             self.size += 1
+        else:
+            print('error')
             
     def push_back(self, item):
         if self.size != self.max_items:
             self.last = (self.last + 1) % self.max_items
             self.items[self.last] = item
             self.size += 1
+        else:
+            print('error')
 
     def pop_front(self):
         if self.is_empty():
+            print('error')
             return None
         self.first = (self.first + 1) % self.max_items
         item = self.items[self.first]
+        print(item)
         self.items[self.first] = None
         self.size -= 1
         return item
 
     def pop_back(self):
         if self.is_empty():
+            print('error')
             return None
         item = self.items[self.last]
+        print(item)
         self.items[self.last] = None
         self.last = (self.last - 1) % self.max_items
         self.size -= 1
         return item
 
+
+def read_input_data():
+    input_data = dict()
+    commands_num = int(input())
+    input_data['deque_length'] = int(input())
+    input_data['commands'] = []
+    for number in range(commands_num):
+        command = input().split(' ')
+        input_data['commands'].append(command)
+    return input_data
+
 if __name__ == '__main__':
-    deque_length = int(input())
-    # commands_number = int(input())
-    d = Deque(deque_length)
-    print(d.items)
-    # for number in range(commands_number):
-    #     command = input().split(' ')
-    #     method = getattr(d, command[0])
-    #     method(int(command[1])) if len(command) == 2 else method()
-    # print(d.items)
-    # d.pop_back(None)
-    [d.push_front(elem) for elem in range(2)]
-    [d.push_back(elem) for elem in range(3)]
-    print(d.items)
-    [d.pop_back() for ind in range(2)]
-    print(d.items)
-    [d.pop_front() for ind in range(2)]
-    print(d.items)
-    d.push_front(10)
-    d.push_back(11)
-    print(d.items)
-    # method = getattr(d, 'pop_back')
-    # method()
-    # print(d.items)
+    input_data = read_input_data()
+    d = Deque(input_data['deque_length'])
+    for command in input_data['commands']:
+        method = getattr(d, command[0])
+        method(int(command[1])) if len(command) == 2 else method()
