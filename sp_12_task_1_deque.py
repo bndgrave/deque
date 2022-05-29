5826616cb8523e27da93ef15a32f29e8939e85a4
Дмитрий Ошмарин <bndgrave1@gmail.com>
2022-05-29 16:15:19 +0500

    Commit 4: Deque via circle buffer arranged

--- sp_12_task_1_deque.py
+++ sp_12_task_1_deque.py
@@ -1,33 +1,65 @@
 class Deque:
-    def __init__(self):
-        self.items = []
+    def __init__(self, deque_length):
+        self.items = [None] * deque_length
+        self.max_items = deque_length
+        self.first = 0
+        self.last = 0
+        self.size = 0
+    
+    def is_empty(self):
+        return self.size == 0
 
     def push_front(self, item):
-        self.items.insert(0, item)
-
+        if self.size != self.max_items:
+            self.items[self.first] = item
+            self.first = (self.first - 1) % self.max_items
+            self.size += 1
+            
     def push_back(self, item):
-        self.items.append(item)
+        if self.size != self.max_items:
+            self.last = (self.last + 1) % self.max_items
+            self.items[self.last] = item
+            self.size += 1
 
     def pop_front(self):
-        try:
-            print(self.items.pop())
-        except IndexError:
-            print('Удаление невозможно, дек пуст!')
+        if self.is_empty():
+            return None
+        self.first = (self.first + 1) % self.max_items
+        item = self.items[self.first]
+        self.items[self.first] = None
+        self.size -= 1
+        return item
 
     def pop_back(self):
-        try:
-            print(self.items.pop())
-        except IndexError:
-            print('Удаление невозможно, дек пуст!')
-
-# def main(input_data):
-#     pass
+        if self.is_empty():
+            return None
+        item = self.items[self.last]
+        self.items[self.last] = None
+        self.last = (self.last - 1) % self.max_items
+        self.size -= 1
+        return item
 
 if __name__ == '__main__':
-    d = Deque()
+    deque_length = int(input())
+    # commands_number = int(input())
+    d = Deque(deque_length)
     print(d.items)
+    # for number in range(commands_number):
+    #     command = input().split(' ')
+    #     method = getattr(d, command[0])
+    #     method(int(command[1])) if len(command) == 2 else method()
+    # print(d.items)
+    # d.pop_back(None)
     [d.push_front(elem) for elem in range(2)]
-    [d.push_back(elem) for elem in range(2)]
+    [d.push_back(elem) for elem in range(3)]
     print(d.items)
-    [d.pop_back() for ind in range(len(d.items)+1)]
+    [d.pop_back() for ind in range(2)]
     print(d.items)
+    [d.pop_front() for ind in range(2)]
+    print(d.items)
+    d.push_front(10)
+    d.push_back(11)
+    print(d.items)
+    # method = getattr(d, 'pop_back')
+    # method()
+    # print(d.items)
